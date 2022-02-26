d3d509d686a55164dbf01f28931856b000f95e7b
Student <student@Codes-MBP.routerlogin.net>
2022-02-25 16:45:40 -0800

    Help_Find_Back, updated credentials for twitter

--- app/controllers/api/v1/twitter_controller.rb
+++ app/controllers/api/v1/twitter_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::TwitterController < ApplicationController
 
     uri = URI("https://api.twitter.com/2/tweets/search/recent?query=missing%20person")
     @req = Net::HTTP::Get.new(uri)
-    @req['Authorization'] = "Bearer AAAAAAAAAAAAAAAAAAAAABXxZAEAAAAAHcdKR3uB5I2%2BgC%2FMZ%2BVrgWV49ag%3DK1yH0siCwoV84qiepRSaEtUBTOKfUiSXlddvTXsBtxKn91XNH3"
+    @req['Authorization'] = "#{Rails.application.credentials.devise[:twitter_secret_key]}"
 
     @res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http|
       http.request(@req)
