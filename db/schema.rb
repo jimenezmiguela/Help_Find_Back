4ce0c46bcde0ef9ddcee6ba3aa73e3e84729142f
inlibra-cpa <inlibra@gmail.com>
2022-03-28 18:02:36 -0400

    switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_22_024155) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "exp", null: false
