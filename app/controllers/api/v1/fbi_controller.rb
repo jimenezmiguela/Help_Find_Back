6bcb762bc3b91c9782f44358bced3b34442f6d39
olegkolyaka78 <oleg.kolyaka@gmail.com>
2022-03-29 23:02:50 -0400

    Addedkeywords missingkids to Twitter API request

--- app/controllers/api/v1/fbi_controller.rb
+++ app/controllers/api/v1/fbi_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::FbiController < ApplicationController
     require 'uri'
     require 'net/http'
 
-    uri = URI('https://api.fbi.gov/@wanted?pageSize=6&page=1&sort_on=modified&sort_order=desc&person_classification=victim')
+    uri = URI('https://api.fbi.gov/@wanted?pageSize=100&page=1&sort_on=modified&sort_order=desc&person_classification=victim')
     @res = Net::HTTP.get_response(uri)
     puts @res.body if @res.is_a?(Net::HTTPSuccess)
 
